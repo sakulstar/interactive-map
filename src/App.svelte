4cb11a9eeb3413e68cdbb6b60ef3541d1c30ab55
sakulstar <111194775+sakulstar@users.noreply.github.com>
2024-02-06 08:55:15 +0100

    converted to svelte from sveltekit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import svelteLogo from './assets/svelte.svg'
+    import viteLogo from '/vite.svg'
+</script>
+
+<main>
+    <p class="text-orange-500 text-3xl">
+        Click on the Vite and Svelte logos to learn more
+    </p>
+</main>
