a5bae2ff72b38ccbef801ea17a51168fd47fe00f
sakulstar <111194775+sakulstar@users.noreply.github.com>
2024-02-08 16:27:05 +0100

    Finished implementing comparisons

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <main class="relative">
-    <div class="flex flex-row ml-5 text-gray-900 font-medium select-none absolute top-0 right-0">
-        <label for="contriesRadioButton" class="mt-2 py-1 px-2 text-white font-semibold rounded-lg {toggleTab ? 'bg-orange-500' : 'bg-orange-300'}">
+    <div class="flex flex-row ml-5 text-gray-900 font-medium select-none absolute top-1 right-1">
+        <label for="contriesRadioButton" class="mt-2 py-1 px-2 text-white font-semibold rounded-lg {toggleTab ? 'bg-orange-500' : 'bg-orange-500/50'}">
             <input id=contriesRadioButton name="tabToggle" type="radio" value={true} bind:group={toggleTab} class="hidden" />
             Lande
         </label><br>
-        <label for="comparisonsRadioButton" class="mx-4 mt-2 py-1 px-2 text-white font-semibold rounded-lg {!toggleTab ? 'bg-orange-500' : 'bg-orange-300'}">
+        <label for="comparisonsRadioButton" class="mx-4 mt-2 py-1 px-2 text-white font-semibold rounded-lg {!toggleTab ? 'bg-orange-500' : 'bg-orange-500/50'}">
             <input id="comparisonsRadioButton" name="tabToggle" type="radio" value={false} bind:group={toggleTab} class="hidden" />
             Sammenligninger
         </label>
--- src/lib/Comparisons.svelte
+++ src/lib/Comparisons.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <main>
-    <div>
+    {#each content["comparisons"] as comparison}
         <p class="text-orange-500 text-3xl font-semibold mt-2 ml-3">
-            Image Comparison 1
+            {comparison["title"]}
         </p>
         <div class="flex flex-row">
-            <!-- <img src="content["comparisons"][index]["image2"]" class=" w-[650px] rounded-l-lg mt-2 ml-2"  alt="1" />
-            <img src={content["comparisons"][index]["image2"]} class=" w-[650px] rounded-r-lg mt-2 mr-2" alt="2" /> -->
+            <img src={comparison["image1"]} class=" w-[650px] rounded-l-lg mt-2 ml-2" alt="1" />
+            <img src={comparison["image2"]} class=" w-[650px] rounded-r-lg mt-2 mr-2" alt="2" />
         </div>
-    </div>
+    {/each}
 </main>
